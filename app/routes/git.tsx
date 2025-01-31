593cec845c2e489edf6d36582859d344e3648937
PlaySlice <mikem8622@icloud.com>
2025-01-31 03:11:36 +0200

    changed titles and pictures

--- app/routes/git.tsx
+++ app/routes/git.tsx
@@ -7,7 +7,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Ez1.dev' }, { name: 'description', content: 'Talk with Ez1' }];
 };
 
 export async function loader(args: LoaderFunctionArgs) {
