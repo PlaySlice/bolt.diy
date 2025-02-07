09962c15c416793141447f9af243d045eca366e2
PlaySlice <mikem8622@icloud.com>
2025-02-07 18:10:03 +0200

    qwen2.5

--- app/lib/modules/llm/providers/open-router.ts
+++ app/lib/modules/llm/providers/open-router.ts
@@ -34,12 +34,12 @@ export default class OpenRouterProvider extends BaseProvider {
       maxTokenAllowed: 8000,
     },
     {
-      name: 'deepseek/deepseek-r1:free',
-      label: 'Deepseek',
+    name: 'qwen/qwen-2.5-coder-32b-instruct',
+      label: 'qwen-2.5-coder',
       provider: 'OpenRouter',
-      maxTokenAllowed: 4096,
+      maxTokenAllowed: 8000,
     },
-/*    {
+/*   {
       name: 'nvidia/llama-3.1-nemotron-70b-instruct:free',
       label: 'llama-3.1-nemotron-70b-instruct (OpenRouter)',
       provider: 'OpenRouter',
