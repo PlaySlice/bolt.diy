4997ec4f681e34aedd6bec2daefa884b6ba797e7
PlaySlice <mikem8622@icloud.com>
2025-02-07 02:57:21 +0200

    Added themeSwitch

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,13 +4,14 @@ import { chatStore } from '~/lib/stores/chat';
 import { classNames } from '~/utils/classNames';
 import { HeaderActionButtons } from './HeaderActionButtons.client';
 import { ChatDescription } from '~/lib/persistence/ChatDescription.client';
+import { ThemeSwitch } from '../ui/ThemeSwitch';
 
 export function Header() {
   const chat = useStore(chatStore);
 
   return (
     <header
-      className={classNames('flex items-center p-5 border-b h-[var(--header-height)]', {
+      className={classNames('flex justify-between items-center p-5 border-b h-[var(--header-height)]', {
         'border-transparent': !chat.started,
         'border-bolt-elements-borderColor': chat.started,
       })}
@@ -23,6 +24,7 @@ export function Header() {
           <img src="/logo-dark-styled.png" alt="logo" className="w-[90px] inline-block hidden dark:block" />
         </a>
       </div>
+      {!chat.started && <div className="mr-1.5"><ThemeSwitch /></div>}
       {chat.started && ( // Display ChatDescription and HeaderActionButtons only when the chat has started.
         <>
           <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
