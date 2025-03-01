cae5942259b59bcda212ae7338762af9cfed30cd
PlaySlice <mikem8622@icloud.com>
2025-03-01 17:56:57 +0200

    adding open ai

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ import { Header } from '~/components/header/Header';
 import BackgroundRays from '~/components/ui/BackgroundRays';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'ez1.dev' }, { name: 'description', content: 'Talk with ez1' }];
+  return [{ title: 'ez1.dev' }, { name: 'description', content: 'Dream it, Build it' }];
 };
 
 export const loader = () => json({});
