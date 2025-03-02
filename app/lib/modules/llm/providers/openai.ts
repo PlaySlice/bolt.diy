7e8f815c4e830da71dd6ae870b37db1e04780c9f
PlaySlice <mikem8622@icloud.com>
2025-03-02 21:44:53 +0200

    fixing

--- app/lib/modules/llm/providers/openai.ts
+++ app/lib/modules/llm/providers/openai.ts
@@ -13,7 +13,6 @@ export default class OpenAIProvider extends BaseProvider {
   };
 
   staticModels: ModelInfo[] = [
-/*     { name: 'gpt-4.5-preview-2025-02-27', label: 'GPT-4.5', provider: 'OpenAI', maxTokenAllowed: 8000 }, */
     { name: 'gpt-4o', label: 'GPT-4o', provider: 'OpenAI', maxTokenAllowed: 8000 },
     { name: 'gpt-4o-mini', label: 'GPT-4o Mini', provider: 'OpenAI', maxTokenAllowed: 8000 },
     { name: 'gpt-4-turbo', label: 'GPT-4 Turbo', provider: 'OpenAI', maxTokenAllowed: 8000 },
